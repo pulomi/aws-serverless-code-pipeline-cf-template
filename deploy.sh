18d42555b018bde8eff035724986d60ea54b2bd3
sniop.safe@gmail.com <4bmnRq9p>
2020-10-03 16:37:54 +0100

    debugging deployment issue

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,7 @@ npm install serverless-dynamodb-local serverless-offline serverless-stack-output
 
 echo "Deploying to $env"
 echo "_______________________________"
-serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/artifacts/$env -v
+export SLS_DEBUG=*
+printenv
+echo "serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/artifacts/$env -v"
+sls deploy --stage $env --package $CODEBUILD_SRC_DIR/artifacts/$env -v
